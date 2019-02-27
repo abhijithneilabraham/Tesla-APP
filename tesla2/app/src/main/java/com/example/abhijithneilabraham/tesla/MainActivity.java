c0986c9e190ada1a4ab7f135677af93d0d4b0c52
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-02-28 00:42:29 +0530

    update

--- tesla2/app/src/main/java/com/example/abhijithneilabraham/tesla/MainActivity.java
+++ tesla2/app/src/main/java/com/example/abhijithneilabraham/tesla/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.abhijithneilabraham.tesla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
 }
