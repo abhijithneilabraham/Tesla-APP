950b3524e0d7b0d1e1efc62a9dea63cd932b2bb7
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-02 00:41:58 +0530

    idk

--- tesla2/app/src/main/java/com/example/abhijithneilabraham/tesla/MainActivity.java
+++ tesla2/app/src/main/java/com/example/abhijithneilabraham/tesla/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.abhijithneilabraham.tesla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button button = findViewById(R.id.button1);
+        final ViewGroup transitionsContainer = (ViewGroup) findViewById(R.id.transitioned);
+        final TextView text = (TextView) transitionsContainer.findViewById(R.id.text1);
+        final Button button = (Button) transitionsContainer.findViewById(R.id.button1);;
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
+                
+
                 setContentView(R.layout.activity_main2);
 
 
