f2e81d248432cec6bb1a276825c6720a552981d2
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-02 01:52:16 +0530

    vattayi

--- tesla2/app/src/main/java/com/example/abhijithneilabraham/tesla/Main2Activity.java
+++ tesla2/app/src/main/java/com/example/abhijithneilabraham/tesla/Main2Activity.java
@@ -6,39 +6,37 @@ import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
 import android.widget.Button;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+    Button b2,b3,b4,b5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        final Button button2=findViewById(R.id.Button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        b4 = (Button) findViewById(R.id.button4);
+        b5 = (Button) findViewById(R.id.button5);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button2:
                 setContentView(R.layout.activity_main3);
-            }
-        });
-        final Button button3=findViewById(R.id.button3);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            case R.id.button3:
                 setContentView(R.layout.activity_main4);
-            }
-        });
-        final Button button4=findViewById(R.id.button4);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            case R.id.button4:
                 setContentView(R.layout.activity_main5);
-            }
-        });
-        final Button button5=findViewById(R.id.button5);
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            case R.id.button5:
                 setContentView(R.layout.activity_main6);
-            }
-        });
+        }
+
     }
 }
--- tesla2/app/src/main/java/com/example/abhijithneilabraham/tesla/MainActivity.java
+++ tesla2/app/src/main/java/com/example/abhijithneilabraham/tesla/MainActivity.java
@@ -1,28 +1,28 @@
 package com.example.abhijithneilabraham.tesla;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.transition.Scene;
-import android.transition.TransitionManager;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+Button b2,b3,b4,b5;
+Context con;
+View.OnClickListener mylistener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ViewGroup transitionsContainer = (ViewGroup) findViewById(R.id.transitioned);
-        final TextView text = (TextView) transitionsContainer.findViewById(R.id.text1);
-        final Button button = (Button) transitionsContainer.findViewById(R.id.button1);;
+
+
+        final Button button = findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                
 
                 setContentView(R.layout.activity_main2);
 
