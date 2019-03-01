b1ded7790cf98db2916427875232194cb27a1b9b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-02 03:18:14 +0530

    updates

--- tesla2/app/src/main/java/com/example/abhijithneilabraham/tesla/MainActivity.java
+++ tesla2/app/src/main/java/com/example/abhijithneilabraham/tesla/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.abhijithneilabraham.tesla;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.transition.Scene;
@@ -9,9 +10,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Button b2,b3,b4,b5;
-Context con;
-View.OnClickListener mylistener;
+//Button b2,b3,b4,b5;
+//Context con;
+//View.OnClickListener mylistener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +23,15 @@ View.OnClickListener mylistener;
         final Button button = findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                //setContentView(R.layout.activity_main2);
+                Intent i1=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i1);
 
-
-                setContentView(R.layout.activity_main2);
 
 
 
             }
         });
+
     }
 }
